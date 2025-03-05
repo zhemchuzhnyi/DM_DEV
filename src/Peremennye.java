e320b87704c916343302144eff2cd55b6c2a1696
zhemchuzhnyi <a_reshkin@inbox.ru>
2025-03-05 22:30:40 +0300

    Lesson2 - "Переменные"

---
+++ src/Peremennye.java
@@ -0,0 +1,20 @@
+public class Peremennye {
+
+    public static void main(String[] args) {
+        int value = 26;
+        System.out.println(value);
+
+        double value2 = 78.123;
+        System.out.println(value2);
+
+        boolean booleanValue = true;
+        System.out.println(booleanValue);
+
+        char testValue = 'a';
+        System.out.println(testValue);
+
+        testValue = 't';
+        System.out.println(testValue);
+
+    }
+}
