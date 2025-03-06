585345239d61f2f8e9cd2c9f3576044496dc367a
zhemchuzhnyi <a_reshkin@inbox.ru>
2025-03-06 22:27:25 +0300

    Lesson 3 - Операции с переменными

--- src/Lesson3Example.java
+++ src/Lesson3Example.java
@@ -12,6 +12,17 @@ public class Lesson3Example {
         double sum1 = v / (double) vv; // приведение типов из инт в дабл, когда необходимо перевести в другой тип переменную - в скобках указывается тип переменной к которой необходимо привести
         System.out.println(sum1);
 
+        int va = 5;
+        va += 25;
+        System.out.println(va);
+
+        int val = 10;
+        val++; //инкремент +1 к переменной
+        System.out.println(val);
+        val--; // декремент -1 от переменной
+        System.out.println(val);
+
+
 
 
 
