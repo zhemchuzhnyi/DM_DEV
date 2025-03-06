201ef6e3438f17006f09db55b2aab3caac384401
zhemchuzhnyi <a_reshkin@inbox.ru>
2025-03-06 22:13:10 +0300

    Lesson 3 - Операции с переменными

---
+++ src/Lesson3Example.java
@@ -0,0 +1,13 @@
+public class Lesson3Example {
+
+    public static void main(String[] args) {
+        int value = 5;
+        int value2 = 3;
+        int sum = value + value2;
+        System.out.println(sum);
+
+
+
+
+    }
+}
