a7030443f4690c7b801a9afc349728c327458954
zhemchuzhnyi <a_reshkin@inbox.ru>
2025-03-10 22:25:23 +0300

    Lesson4 - "Switch"

---
+++ src/com/dmdev/lesson4/SwitchExample.java
@@ -0,0 +1,25 @@
+package com.dmdev.lesson4;
+
+public class SwitchExample {
+
+    public static void main(String[] args) {
+        // byte, short, int, char, String, enum
+        int value = 2;
+        switch (value){
+            case 1:
+                System.out.println("value == 1");
+                break;
+            case 2:
+                System.out.println("value == 2");
+                break;
+            case 10:
+                System.out.println("value == 10");
+                break;
+            case 28:
+                System.out.println("value == 28");
+                break;
+            default:
+                System.out.println("Нет такого значения");
+        }
+    }
+}
