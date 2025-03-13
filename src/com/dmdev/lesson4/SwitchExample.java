8cb240ea985bba77587828a661b231d71df197de
Otus_PC <a_reshkin@inbox.ru>
2025-03-13 22:32:10 +0300

    Update

--- src/com/dmdev/lesson4/SwitchExample.java
+++ src/com/dmdev/lesson4/SwitchExample.java
@@ -12,7 +12,7 @@ public class SwitchExample {
             case 2:
                 System.out.println("value == 2");
                 break;
-            case 110:
+            case 10:
                 System.out.println("value == 10");
                 break;
             case 28:
