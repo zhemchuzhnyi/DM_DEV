b3a78f3f6170aff5a12ae5c51d7dfae5992b17c3
zhemchuzhnyi <a_reshkin@inbox.ru>
2025-03-14 18:52:24 +0300

    Update

--- src/com/dmdev/lesson4/SwitchExample.java
+++ src/com/dmdev/lesson4/SwitchExample.java
@@ -15,7 +15,7 @@ public class SwitchExample {
             case 10:
                 System.out.println("value == 10");
                 break;
-            case 28:
+            case 228:
                 System.out.println("value == 28");
                 break;
             default:
