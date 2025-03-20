cf32f78f8f41fe5cfb7cd2244ad8aa69ecc4dbbf
Otus_PC <a_reshkin@inbox.ru>
2025-03-20 22:44:07 +0300

    If - Else Practice

--- src/com/dmdev/task/Task2.java
+++ src/com/dmdev/task/Task2.java
@@ -1,4 +1,17 @@
 package com.dmdev.task;
 
+/**
+ * Дано целое число.
+ * Определить, является ли последняя цифра этого числа - цифрой 3
+ */
+
 public class Task2 {
+
+    public static void main(String[] args) {
+        // необходимо поделить по модулю, интересет остаток
+        int testValue = 912312;
+        if (testValue % 10 == 3) {
+            System.out.println("Последняя цифра числа является 3");
+        }
+    }
 }
