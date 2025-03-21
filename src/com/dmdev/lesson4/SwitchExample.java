63e32f7dbdef1b6be87e38cc1d99334c63b1e92f
Otus_PC <a_reshkin@inbox.ru>
2025-03-21 13:12:16 +0300

    Update

--- src/com/dmdev/lesson4/SwitchExample.java
+++ src/com/dmdev/lesson4/SwitchExample.java
@@ -5,12 +5,25 @@ public class SwitchExample {
     public static void main(String[] args) {
         // byte, short, int, char, String, enum
         int value = 1;
-        switch (value){
+        char charValue = switch (value) {
+            case 1 -> '1';
+        case 2, 3, 4 -> '2';
+        default -> '0';
+        };
+        switch (value) {
+            case 1-> System.out.println("value == 1");
+            case 2, 3, 4 -> System.out.println("value == 2, 3, 4");
+
+        default -> System.out.println("Нет такого значения");
+        }
+        switch (value) {
             case 1:
                 System.out.println("value == 1");
                 break;
             case 2:
-                System.out.println("value == 2");
+            case 3:
+            case 4:
+                System.out.println("value == 2, 3, 4");
                 break;
             case 10:
                 System.out.println("value == 10");
