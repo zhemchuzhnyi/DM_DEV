340a6548ac1255034967ff2ab11aa4726c51e577
Otus_PC <a_reshkin@inbox.ru>
2025-03-24 23:56:47 +0300

    Update

---
+++ src/com/dmdev/lesson6/WhileExample.java
@@ -0,0 +1,12 @@
+package com.dmdev.lesson6;
+
+// циклы нужны  для повторения одного и того же действия (кусочка кода) - несколько раз, до тех пор пока условие верно
+//
+
+public class WhileExample {
+
+    public static void main(String[] args) {
+        int value = 10;
+
+    }
+}
