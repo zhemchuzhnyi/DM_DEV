36f85171e0fe9c951527a4f1865c06410511efc9
Otus_PC <a_reshkin@inbox.ru>
2025-03-25 00:18:59 +0300

    Цикл While

--- src/com/dmdev/lesson6/WhileExample.java
+++ src/com/dmdev/lesson6/WhileExample.java
@@ -6,6 +6,13 @@ package com.dmdev.lesson6;
 public class WhileExample {
 
     public static void main(String[] args) {
+        int value = 0;
+        do {
+            System.out.println(value);
+        } while (value < 10); // цикл с постусловием - выполняется 1 раз
+    }
+
+    private static void whileExample() {
         int value = 10;
         while (value > 0) {
             System.out.println(value);
