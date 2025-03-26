fea441dc64651be0839f9733f7c9626ccada50e2
Otus_PC <a_reshkin@inbox.ru>
2025-03-26 22:00:09 +0300

    Цикл For

---
+++ src/com/dmdev/lesson6/ForExample.java
@@ -0,0 +1,18 @@
+package com.dmdev.lesson6;
+
+public class ForExample {
+
+    public static void main(String[] args) {
+        for (int value = 10; value > 0; value--) { // for состоит из 3х частей
+            System.out.println(value);
+            if (value == 5) {
+                continue; // - возвращается к началу условия
+               // break; // - доходит до 5
+            }
+        }
+        // 1я- инициализация в скобках, 2я- условие (value > 0), 3я- изменение\превращение\уменьшение переменной value
+        // плюсы for - можнео использовать лайф темплейты - скорость
+
+
+    }
+}
