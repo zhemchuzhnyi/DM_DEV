7264acee3d1cfea8787357d464fe47fd3fe6dd2b
Otus_PC <a_reshkin@inbox.ru>
2025-04-01 23:13:37 +0300

    Practice do - while

--- src/com/dmdev/task/Task7.java
+++ src/com/dmdev/task/Task7.java
@@ -9,6 +9,21 @@ package com.dmdev.task;
 public class Task7 {
 
     public static void main(String[] args) {
+        double sourceSum = 24;
+        double result = sum(sourceSum);
+        System.out.println(result);
 
+
+    }
+
+    public static double sum(double sourceSum) {
+        double result = sourceSum;
+        for (int year = 1626 + 1; year <= 2025; year++){
+            double percent = result * 0.05;
+            result += percent;
+            System.out.println(year + " : " + result);
+
+        }
+        return result;
     }
 }
