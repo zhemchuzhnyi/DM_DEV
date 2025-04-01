cc8f24efffe0ec2b2808a5268adb1e8caf19cfcf
Otus_PC <a_reshkin@inbox.ru>
2025-04-01 23:21:24 +0300

    Practice do - while

---
+++ src/com/dmdev/task/Task8.java
@@ -0,0 +1,29 @@
+package com.dmdev.task;
+
+/**
+ * Написать программу определяющую является ли введенное число простым
+ * т.е делится без остатка только на 1 и само на себя
+ */
+
+public class Task8 {
+
+    public static void main(String[] args) {
+        int value = 11;
+        boolean result = isSimple(value);
+        System.out.println(result);
+
+
+    }
+
+    public static boolean isSimple(int value) {
+        boolean result = true;
+        for (int i = 2; i < value; i++) {
+            if (value % i == 0) {
+                result = false;
+                break;
+            }
+        }
+
+        return result;
+    }
+}
