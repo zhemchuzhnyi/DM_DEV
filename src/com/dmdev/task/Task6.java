c644f6dbf4e99f6d5bfdce65a04bad2a903a29ae
Otus_PC <a_reshkin@inbox.ru>
2025-04-01 22:50:01 +0300

    Practice do - while

--- src/com/dmdev/task/Task6.java
+++ src/com/dmdev/task/Task6.java
@@ -1,4 +1,42 @@
 package com.dmdev.task;
 
+/*
+Имеется число - посчитать сумму цифр данного числа
+ */
+
 public class Task6 {
+
+    public static void main(String[] args) {
+        int value = 123456;
+        int result = sum(value);
+        System.out.println(result);
+
+        int result1 = sumFor(value);
+        System.out.println(result1);
+
+    }
+
+    public static int sum(int value) {
+        int result = 0;
+        int currentValue = value;
+        while (currentValue != 0) {
+            result += currentValue % 10;
+            currentValue /= 10;
+        }
+
+
+
+        return result;
+    }
+
+    public static int sumFor(int value) {
+        int result = 0;
+        for (int currentValue = value; currentValue != 0 ; currentValue /= 10) {
+            result += currentValue % 10;
+        }
+
+
+
+        return result;
+    }
 }
